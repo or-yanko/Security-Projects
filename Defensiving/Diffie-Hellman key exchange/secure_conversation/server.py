9673ee79910d43261bb82c2c6bf778e37345b9be
or yanko <54112968+or-yanko@users.noreply.github.com>
2022-10-07 21:17:45 +0300

    Update server.py

--- Defensiving/Diffie-Hellman key exchange/secure_conversation/server.py
+++ Defensiving/Diffie-Hellman key exchange/secure_conversation/server.py
@@ -1,4 +1,4 @@
 import funcs_for_diffy_hellman_key_exchange as f
 socket, address = f.socket_after_connection_listen_as_a_server(
     f.get_my_ip_address())
-print(f.recive_msg_with_diffy_hellman_key_exchange(socket))
+print(f.recive_msg_with_diffy_hellman_key_exchange_and_hash_confirmation(socket))
